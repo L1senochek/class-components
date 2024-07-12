49b1010d8b9b8df6b1ec5f8348ce9d873579bb12
L1senochek <lisenochek96@gmail.com>
2024-07-12 21:15:55 +0300

    feat: add main tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,20 @@
-import { expect, it, test } from 'vitest';
+import { describe, expect, it, test } from 'vitest';
 import { ReactElement } from 'react';
+import { act, render } from '@testing-library/react';
+import App from './App';
+
+describe('App: ', (): void => {
+  it('Renders headline.', async () => {
+    await act(async (): Promise<void> => {
+      render(<App />);
+      await Promise.resolve();
+    });
+  });
+  it('Renders TodoApp component.', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Items per page/i)).toBeTruthy();
+  });
+});
 
 test('adds 1 + 2 to equal 3', () => {
   const a = 1;
